2185ebbebcb05e6a0940676aa3da25e69c54e7a8
Georgii_Belinskii <georgij.belinsky@yandex.ru>
2022-10-03 09:34:56 +0300

    added check_anomaies function (calculation and visualisation)

--- detector_of_anomalies.py
+++ detector_of_anomalies.py
@@ -52,8 +52,68 @@ def detector_of_anomalies():
         return df
 
     @task
+    @task
     def check_anomalies_iqr(df, window=5, number_of_iqr=2, critical_level=0.1):
-        pass
+        # calculation 25percentile, 75 percentile and IQR for last 5 values\window
+
+        # df - dataframe
+        # column - calculated column from dataframe
+        # window - period\window for moving average
+        # number_of_iqr  - number of IQR range
+        # critical_level - sensitivity of system, percent of difference from previous value
+        for column in df.columns[1:]:
+
+            # percentiles of moving average and inter quantile range
+            df['m_25p'] = df[column].shift(1).rolling(window).quantile(0.25)
+            df['m_75p'] = df[column].shift(1).rolling(window).quantile(0.75)
+            df['iqr'] = df.m_75p - df.m_25p
+
+            # calculation up and low boarder of iqr
+            df['up'] = df['m_75p'] + number_of_iqr * df['iqr']
+            df['up'] = df.up.rolling(window).mean()
+
+            df['low'] = df['m_25p'] - number_of_iqr * df['iqr']
+            df['low'] = df.low.rolling(window).mean()
+
+            # last and previous values, which will be comparing
+            last = df.iloc[-1][column]
+            prev = df[column].iloc[-2]
+
+            if ((last < df.low.iloc[-1]) or (last > df.up.iloc[-1])) and (abs((last / prev) - 1)) > critical_level:
+                msg_alert = f''' ALERT!!! IQR-system
+    time: {df.period.iloc[-1]}
+    measure: {column}
+    current value: {last}
+    prev value: {prev}
+    deviation from previous value {((last / prev) - 1):.2%}
+    look up here: link_dashboard
+        '''
+                bot.sendMessage(chat_id=my_chat_id, text=msg_alert)
+
+                sns.set_theme(style="whitegrid")
+                sns.axes_style("darkgrid")
+                fig, ax = plt.subplots(figsize=(17, 12))
+                dict_colors = {'up': 'red',
+                               'low': 'red'}
+
+                for el in [column, 'up', 'low']:
+                    ax = sns.lineplot(
+                        x='period',
+                        y=el,
+                        data=df,
+                        color=dict_colors.get(el),
+                        label=el)
+                ax.set_title(f'{column}')
+
+                plot_object = io.BytesIO()
+                ax.figure.savefig(plot_object)
+                plot_object.seek(0)
+                plot_object.name = f'{column}_anomaly_report.png'
+                plt.close()
+                bot.sendPhoto(chat_id=my_chat_id, photo=plot_object)
+
+
+
 
 
 
